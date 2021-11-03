7ec96685333b5d2572819efa5527deb8d6137c97
20A95A3503 <93142101+20A95A3503@users.noreply.github.com>
2021-11-03 11:50:52 +0530

    Add files via upload

---
+++ natural numbers.c
@@ -0,0 +1,11 @@
+/*area and perimeter of the square*/
+#include<stdio.h>
+void main()
+{
+	int s,a,p;
+	s=2;
+	a=s*s;
+	p=4*s;
+	printf("area of the square with side %d is %d\n",s,a);
+	printf("perimeter of the square with side %d is %d",s,p);
+}
